0ee8bb9189c3f21af3311a6d77378256990c1265
Son Ho <hosonmarc@gmail.com>
2025-01-24 11:37:04 +0000

    Add the intermediate spec and proofs about the inverse NTT

--- proofs/Symcrust/SpecAux.lean
+++ proofs/Symcrust/SpecAux.lean
@@ -10,6 +10,8 @@ namespace Symcrust.SpecAux
 
 open Symcrust.Spec
 
+open Aeneas.SRRange
+
 /- Introduce auxiliary definitions to isolate the different loops inside the target specification -/
 namespace Target
 
@@ -50,6 +52,38 @@ namespace Target
     simp only [Id.run]
     rfl
 
+  def invNttLayerInner (f : Polynomial) (i len start : Nat) : Polynomial := Id.run do
+    let mut f := f
+    let zeta := ζ ^(bitRev 7 i)
+    for j in [start:start+len] do
+      let t := f[j]!
+      f := f.set j (t + f[j + len]!)
+      f := f.set (j + len) (zeta * (f[j + len]! - t))
+    pure f
+
+  def invNttLayer (f : Polynomial) (i len : Nat) (hLen : 0 < len := by assumption) : Polynomial × Nat := Id.run do
+    let mut f := f
+    let mut i := i
+    for start in [0:256:2*len] do
+      let i0 := i
+      i := i - 1
+      f := invNttLayerInner f i0 len start
+    pure (f, i)
+
+  def invNtt (f : Polynomial) : Polynomial := Id.run do
+    let mut fi := (f, 127)
+    let mut i := 127
+    for h: len in [2 : <256 : *= 2] do
+      fi := invNttLayer fi.1 fi.2 len (by simp +zetaDelta [Membership.mem] at *; omega)
+    pure (fi.1 * (3303 : Zq))
+
+  theorem invNtt_eq (f : Polynomial) : invNtt f = Spec.invNtt f := by
+    rw [invNtt, Spec.invNtt]
+    unfold invNttLayer
+    unfold invNttLayerInner
+    simp only [Id.run]
+    rfl
+
 end Target
 
 def nttLayerInner (f : Polynomial) (i : Nat) (len : Nat)
@@ -80,6 +114,35 @@ def ntt (f : Polynomial) : Polynomial :=
   let f := nttLayer f 64 2 0
   f
 
+def invNttLayerInner (f : Polynomial) (i : Nat) (len : Nat)
+  (start : Nat) (j : Nat) : Polynomial :=
+  if j < len then
+    let c0 := f[start + j]!
+    let c1 := f[start + j + len]!
+    let zeta := ζ ^ bitRev 7 i
+    let f := f.set (start + j) (c0 + c1)
+    let f := f.set (start + j + len) (zeta * (c1 - c0))
+    invNttLayerInner f i len start (j + 1)
+  else f
+
+def invNttLayer (f : Polynomial) (i : Nat) (len : Nat) (start : Nat) (hLen : 0 < len := by simp) :
+  Polynomial :=
+  if start < 256 then
+    let f := invNttLayerInner f i len start 0
+    invNttLayer f (i - 1) len (start + 2 * len) hLen
+  else f
+
+def invNtt (f : Polynomial) : Polynomial :=
+  let f := invNttLayer f 127 2 0
+  let f := invNttLayer f 63 4 0
+  let f := invNttLayer f 31 8 0
+  let f := invNttLayer f 15 16 0
+  let f := invNttLayer f 7 32 0
+  let f := invNttLayer f 3 64 0
+  let f := invNttLayer f 1 128 0
+  f * (3303 : Zq)
+
+
 -- TODO: move, also update the neq test
 theorem Polynomial.set_set_neq {i j : Nat} (h : i ≠ j)
   (p : Polynomial) (x y : Zq) :
@@ -89,10 +152,17 @@ theorem Polynomial.get_set_neq {i j : Nat} (h : i ≠ j)
   (p : Polynomial) (x : Zq) :
   (p.set i x).get! j = p.get! j := by sorry
 
+theorem Polynomial.getElem_set_neq {i j : Nat} (h : i ≠ j)
+  (p : Polynomial) (x : Zq) :
+  (p.set i x)[j]! = p[j]! := by
+  apply Polynomial.get_set_neq; assumption
+
 -- TODO: this lemma should exist somewhere
 private theorem fun_eq_arg_eq_imp_eq {α β : Type} (f g : α → β) (x y : α) :
   f = g → x = y → f x = g y := by
   simp +contextual
+
+/-! The proofs about the NTT -/
 
 private theorem nttLayerInner_eq
   (f : Polynomial) (i len start : Nat) :
@@ -142,14 +212,9 @@ private theorem nttLayerInner_eq
         apply fun_eq_arg_eq_imp_eq <;> try rfl
         clear hInd
         -- Working on the interesting part: we need to swap the two updates
-        have h1 := @Polynomial.set_set_neq (start + j) (start + j + len) (by omega)
-        simp only [h1]
+        rw [Polynomial.set_set_neq] <;> try omega
+        rw [Polynomial.getElem_set_neq] <;> try omega
         ring_nf
-        have h2 := @Polynomial.get_set_neq (start + j + len) (start + j) (by omega)
-        simp only [getElem!, dite_true, getElem, h2]
-        ring_nf
-
-open Aeneas.SRRange
 
 private theorem nttLayer_eq_fst_aux (f : Polynomial) (i len start : Nat) (hLenLt : 0 < len) :
   let p : MProd _ _ := foldWhile 256 (2 * len) (by omega) (fun b a => ⟨Target.nttLayerInner b.1 b.2 len a, b.2 + 1⟩) start ⟨f, i⟩
@@ -227,14 +292,152 @@ private theorem nttLayer_eq (f : Polynomial) (len : Nat) (hLen : 0 < len)
   exists len.log2
   tauto
 
-theorem ntt_eq_target (f : Polynomial) :
+private theorem ntt_eq_target (f : Polynomial) :
   ntt f = Target.ntt f := by
   unfold ntt Target.ntt
   simp [Id.run, Aeneas.divRange, Aeneas.divRange.loop]
   repeat (rw [← nttLayer_eq] <;> simp [Nat.log2])
 
+/-- This is the important theorem -/
 theorem ntt_eq (f : Polynomial) :
   ntt f = Spec.ntt f := by
   rw [ntt_eq_target, Target.ntt_eq]
+
+/-! The proofs about the NTT⁻¹ -/
+
+private theorem invNttLayerInner_eq
+  (f : Polynomial) (i len start : Nat) :
+  invNttLayerInner f i len start 0 = Target.invNttLayerInner f i len start
+  := by
+  -- Unfold the definitions and simplify
+  generalize hj : 0 = j
+  unfold Target.invNttLayerInner
+  simp only [Id.run, Id.pure_eq, Id.bind_eq, forIn_eq_forIn_range', size, add_tsub_cancel_left,
+    add_tsub_cancel_right, Nat.div_one, List.forIn_yield_eq_foldl, zero_lt_one, foldl_range',
+    mul_one]
+  have : start = start + j := by simp only [self_eq_add_right, hj]
+  conv => rhs; arg 5; rw [this]
+  clear this
+  -- We do the induction on len - j
+  generalize hSteps : len - j = steps
+  clear hj
+  -- We need this fact (we get it by doing a case disjunction - the case where
+  -- it is false is trivial)
+  dcases hLe : ¬ j ≤ len
+  . -- Simple case: len < j
+    simp_all only [not_le]
+    -- Simplify the lhs
+    have h : ¬ j < len := by omega
+    simp [h, invNttLayerInner, ↓reduceIte, *]
+  . -- Interesting case: j ≤ len
+    revert f
+    revert len j
+    induction steps <;> intro len j hSteps hLe f <;> unfold invNttLayerInner
+    . -- zero
+      have : len = j := by omega
+      simp_all only [tsub_self, le_refl, lt_self_iff_false, ite_false]
+      simp only [lt_self_iff_false, not_false_eq_true, Aeneas.SRRange.foldWhile_id, implies_true]
+    . -- succ
+      rename_i steps hInd
+      dcases hLe' : ¬ j < len
+      . -- Simple case
+        simp only [↓reduceIte, add_lt_add_iff_left, not_false_eq_true, Aeneas.SRRange.foldWhile_id,
+          implies_true, hLe']
+      . -- Recursive case
+        simp only [hLe', ↓reduceIte, add_lt_add_iff_left, foldWhile_step]
+        replace hInd := hInd len (j + 1) (by omega) (by omega)
+        simp only [hInd]
+        -- Several `Polynomial.set` operations are inverted in the continutations
+        -- We first zoom into the interesting terms
+        apply fun_eq_arg_eq_imp_eq <;> try rfl
+        clear hInd
+        -- Working on the interesting part: we need to simplify a write/read
+        rw [Polynomial.getElem_set_neq]; omega
+
+private theorem invNttLayer_eq_fst_aux (f : Polynomial) (i len start : Nat) (hLenLt : 0 < len) :
+  let p : MProd _ _ := foldWhile 256 (2 * len) (by omega) (fun b a => ⟨Target.invNttLayerInner b.1 b.2 len a, b.2 - 1⟩) start ⟨f, i⟩
+  invNttLayer f i len start hLenLt = p.fst := by
+  simp only
+  unfold invNttLayer foldWhile
+  split
+  . rename_i hLt
+    simp only
+    have := invNttLayerInner_eq f i len start
+    rw [this]; clear this
+    have := invNttLayer_eq_fst_aux (Target.invNttLayerInner f i len start) (i - 1) len (start + 2 * len) hLenLt
+    simp only at this
+    rw [this]
+  . simp_all
+
+private theorem invNttLayer_eq_fst_arith
+  (len : Nat) (hLen : ∃ k, k ≤ 7 ∧ len = 2 ^k) :
+  (255 / (2 * len) + 1) * (2 * len) = 256 := by
+  -- We simply brute force the proof by making a case disjunction on k
+  -- TODO: the proof is quite simple but the theorem must be generalized (and the proof is then induction)
+  replace ⟨ k, hLen ⟩ := hLen
+  repeat (cases k <;> simp_all <;> rename_i k <;> try omega)
+
+private theorem invNttLayer_eq_fst (f : Polynomial) (i len : Nat)
+  (hLen : 0 < len) (hkLen : ∃ k, k ≤ 7 ∧ len = 2 ^ k) :
+  invNttLayer f i len 0 hLen = (Target.invNttLayer f i len).fst := by
+  unfold Target.invNttLayer
+  simp only [Id.run, Id.pure_eq, Id.bind_eq, forIn_eq_forIn_range', size, tsub_zero,
+    Nat.succ_add_sub_one, Nat.ofNat_pos, mul_pos_iff_of_pos_left, hLen, Nat.add_div_right,
+    List.forIn_yield_eq_foldl, foldl_range', zero_add]
+  have := invNttLayer_eq_fst_arith len hkLen
+  simp only [this]
+  have := invNttLayer_eq_fst_aux f i len 0 hLen
+  simp only at this
+  simp only [this]
+
+private theorem  invNttLayer_eq_snd_aux
+  (x : α) (f : α → Nat → Nat → α)
+  (i start len step : ℕ):
+  (List.foldl (fun (b : MProd _ _) a => ⟨f b.fst b.snd a, b.snd - 1⟩) ⟨x, i⟩
+    (List.range' start len step)).snd =
+  i - len
+  := by
+  revert x i start
+  induction len <;> intro x i start
+  . simp_all
+  . rename_i len hInd
+    simp_all [List.range']
+    omega
+
+private theorem invNttLayer_eq_snd
+  (f : Polynomial) (i len : Nat) (hLen : 0 < len) :
+  (Target.invNttLayer f i len hLen).snd = i - (255 / (2 * len) + 1) := by
+  unfold Target.invNttLayer
+  simp only [Id.run, Id.pure_eq, Id.bind_eq, Aeneas.SRRange.forIn_eq_forIn_range',
+    Aeneas.SRRange.size, tsub_zero, Nat.succ_add_sub_one, Nat.ofNat_pos, mul_pos_iff_of_pos_left,
+    pow_pos, Nat.add_div_right, List.forIn_yield_eq_foldl, hLen]
+  have := invNttLayer_eq_snd_aux f (fun x y => Target.invNttLayerInner x y len) i
+  simp only [this, add_right_inj]
+
+private theorem invNttLayer_eq_aux (f : Polynomial) (i len : Nat)
+  (hLen : 0 < len) (hkLen : ∃ k, k ≤ 7 ∧ len = 2 ^ k) :
+  (invNttLayer f i len 0 (by simp [hLen]), i - (255 / (2 * len) + 1)) = Target.invNttLayer f i len := by
+  have := invNttLayer_eq_fst f i len hLen
+  have := invNttLayer_eq_snd f i len hLen
+  cases h: Target.invNttLayer f i len
+  simp_all
+
+private theorem invNttLayer_eq (f : Polynomial) (len : Nat) (hLen : 0 < len)
+  (hkLen : len = 2 ^ (Nat.log2 len) ∧ Nat.log2 len ≤ 7) :
+  (invNttLayer f i len 0 hLen, i - (255 / (2 * len) + 1)) = Target.invNttLayer f i len := by
+  rw [invNttLayer_eq_aux]
+  exists len.log2
+  tauto
+
+private theorem invNtt_eq_target (f : Polynomial) :
+  invNtt f = Target.invNtt f := by
+  unfold invNtt Target.invNtt
+  simp [Id.run, Aeneas.mulRange]
+  repeat (rw [← invNttLayer_eq] <;> try simp [Nat.log2])
+
+/-- This is the important theorem -/
+theorem invNtt_eq (f : Polynomial) :
+  invNtt f = Spec.invNtt f := by
+  rw [invNtt_eq_target, Target.invNtt_eq]
 
 end Symcrust.SpecAux
